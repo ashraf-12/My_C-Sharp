303ef66770b3dd62dbe07cb3fb6dc164611e6e2f
ashraf-12 <89690509+ashraf-12@users.noreply.github.com>
2023-07-05 04:56:12 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace My_First_C__Project
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello , This is my first C# Project");
+            string Name = Console.ReadLine();
+            Console.WriteLine("\nYour Name is : %d", Name );
+        }
+    }
+}
